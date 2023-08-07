d43d0d312f97db259c87a490c00deb3f47660e54
guhandelta <guhan.delta@gmail.com>
2023-08-07 15:19:53 -0700

    Converted Astro server running on Static mode, to a SSR server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+/*
+    Until adding this config[output: 'server'], Astro runs in Static mode, and throws 
+        ```Request.formData: Could not parse content as FormData```
+    error.
+    This config is to convert Astro into a SSR server,to handle each req dynamically, which is kindof what HTMX is used to dealing with, like send FormData and get values back, dynamically. 
+*/
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind()]
 });
